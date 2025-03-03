83f348c77f03cea8be10d79cce366d51206d97a0
syed shaheer <sdshaheer5421@gmail.com>
2025-03-03 19:56:55 +0530

    dynamic import

--- app/components/homepage/education/index.jsx
+++ app/components/homepage/education/index.jsx
@@ -1,9 +1,13 @@
+import dynamic from "next/dynamic";
+
 import Image from "next/image";
 
 import { educations } from "@/utils/data/educations";
 import { BsPersonWorkspace } from "react-icons/bs";
 import AnimationLottie from "../../helper/animation-lottie";
-import GlowCard from "../../helper/glow-card";
+// import GlowCard from "../../helper/glow-card";
+const GlowCard = dynamic(() => import("../../helper/glow-card"), { ssr: false });
+
 import lottieFile from '/public/lottie/study.json';
 
 function Education() {
--- app/components/homepage/experience/index.jsx
+++ app/components/homepage/experience/index.jsx
@@ -1,8 +1,11 @@
+import dynamic from "next/dynamic";
+
 import { experiences } from "@/utils/data/experience";
 import Image from "next/image";
 import { BsPersonWorkspace } from "react-icons/bs";
 import AnimationLottie from "../../helper/animation-lottie";
-import GlowCard from "../../helper/glow-card";
+// import GlowCard from "../../helper/glow-card";
+const GlowCard = dynamic(() => import("../../helper/glow-card"), { ssr: false })
 import experience from '/public/lottie/code.json';
 
 function Experience() {
